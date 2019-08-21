3aca702e2cad16b2ce4ffb648ed0ea8b532c9be7
stasbar <stasbar1995@gmail.com>
2019-08-22 01:36:55 +0200

    add testing batteries.json

---
+++ test.py
@@ -0,0 +1,23 @@
+﻿
+import subprocess
+import os
+import json
+from utils import battery_id
+
+with open('batteries.json', 'r') as file: 
+    batteries = json.load(file)
+    for battery in batteries:
+        print('Testing', battery['brand'], battery['model'])
+        assert battery['brand']
+        assert battery['model']
+        assert battery['chemistry']
+        assert battery['size']
+        assert battery['capacity']
+        assert battery['voltage']
+        assert battery['stableCurrent']
+        assert battery['maxVapingCurrent']
+        assert battery['cutOff']
+        assert battery['imgUrl']
+        assert battery['reviewUrl']
+
+
